189462bbc00d7a79d997b53c7e1e711596c00991
jenchieh <jcs090218@gmail.com>
2018-12-02 00:00:50 +0800

    First core.

---
+++ auto-rename-tag.el
@@ -0,0 +1,38 @@
+;;; auto-rename-tag.el --- Automatically rename paired HTML/XML tag.                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Shen, Jen-Chieh
+;; Created date 2018-12-01 23:56:15
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Remind current line status by current buffer.
+;; Keyword: abbreviation customizable shortcut
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.4"))
+;; URL: https://github.com/jcs090218/auto-rename-tag
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Automatically rename paired HTML/XML tag.
+;;
+
+;;; Code:
+
+
+
+(provide 'auto-rename-tag)
+;;; auto-rename-tag.el ends here
