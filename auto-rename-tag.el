96482f04345877fa2ee22e912e0a7333a0f74aaa
Jen-Chieh <jcs090218@gmail.com>
2020-08-27 11:31:09 +0800

    Bump version.

--- auto-rename-tag.el
+++ auto-rename-tag.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Automatically rename paired HTML/XML tag.
 ;; Keyword: auto-complete html rename tag xml
-;; Version: 0.3.0
+;; Version: 0.3.1
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/jcs-elpa/auto-rename-tag
 
